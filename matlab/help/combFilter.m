c6c9139ed54b1e2269563b5274a61d29f4e5e778
Erik Stålberg <mail@erikstalberg.se>
2020-02-11 15:51:30 +0100

    Updated combFilter.m

--- matlab/help/combFilter.m
+++ matlab/help/combFilter.m
@@ -1,17 +1,16 @@
-function Y = combFilter(X, decay, limit)
+function Y = combFilter(X, decay, limits, kmax)
 N = length(X);
 
-if nargin <3
-   limit = N; 
+if (nargin <3) || (length(limits) ~= 2)
+   limits = [1 N]; 
 end
 
 Y = zeros(limit,1);
 
-for f=1:limit
+for f=limits(1):limits(2)
     k=1;
-    while f*k <= N
+    while (f*k <= N) || (k <= kmax)
         Y(f) = Y(f) + X(f*k)*decay.^(k-1);
         k = k+1;
     end
-    %Y(f) = Y(f)/(k+1);
 end
