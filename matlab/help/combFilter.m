99a0773f9f2fdec4584ed683526c9b9b233f4ff4
Erik Stålberg <mail@erikstalberg.se>
2020-02-12 11:41:10 +0100

    Added YIN

--- matlab/help/combFilter.m
+++ matlab/help/combFilter.m
@@ -1,16 +1,23 @@
-function Y = combFilter(X, decay, limits, kmax)
+function Y = combFilter(X, decay, limits, kmax, bias)
 N = length(X);
 
 if (nargin <3) || (length(limits) ~= 2)
    limits = [1 N]; 
 end
+if nargin<4
+   kmax = inf; 
+end
 
-Y = zeros(limit,1);
+inharmonicity = @(k, B) sqrt(1+B*k.^2);
+
+Y = zeros(N,1);
 
 for f=limits(1):limits(2)
     k=1;
-    while (f*k <= N) || (k <= kmax)
-        Y(f) = Y(f) + X(f*k)*decay.^(k-1);
+    while  (k <= kmax) && (f*k <= N)
+        Y(f) = Y(f) + X(round(f*k*inharmonicity(k,bias)))*decay.^(k-1);
         k = k+1;
     end
+end
+
 end
