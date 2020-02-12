22c2cd523719474d42948ff541796755672afe08
Erik Stålberg <mail@erikstalberg.se>
2020-02-12 12:03:39 +0100

    removed stuff

--- matlab/main.m
+++ matlab/main.m
@@ -31,62 +31,9 @@ midinotes(:,5:6) = midinotes(:,5:6) + 3;
 midinotes(:,3) = midi2freq(midinotes(:,3)); % mito notes to frequencies
 qtone = exp((log(midi2freq(69))-log(midi2freq(68)))/2); % quarter tone, aka error margin
 
-%% Spectral estimation for each tone
-voice = 4;
-tones = zeros(sum(midinotes(:,1) == voice),5);
-
-d    = 3; % peaks to look for
-P    = 512;
-ff   = (0:(P-1))/P*fs; 
-cut  = 0.0375;
-
-k = 1;
-for i=1:length(midinotes)
-    if midinotes(i,1) == voice
-        tstart = midinotes(i,5)+cut;
-        tend   = midinotes(i,6)-cut;
-        tindex = round(tstart*fs):round(tend*fs);
-        N      = length(tindex);
-        tones(k,1) = mean([tstart tend]); % Mean time of note
-        tones(k,2) = midinotes(i,3);
-        x = txtcor(tindex,voice); % sound sequence
-        
-        % Relax
-        %[fRelax, ~] = relax(x,d);
-        %tones(k,4) = min(sort(fs*fRelax/2/pi));
-        
-        % Periodogram
-        spect = fftshift(abs(fft(x,P))/N).^2; % periodogram
-        spect = spect(P/2+1:end);
-        if sum(spect) > 1e-8
-            per = combFilter(spect,1,4);
-            tones(k,3) = ff(min(sort(findpeaks(spect,d)))); % estimated base freq
-        end
-        
-        % q-Spice
-%         A  = exp( 2i*pi*(1:N)'*ff );
-%         [p,~,R] = q_SPICE( x, A, 2, 1e-4 );             % q=2
-%         tmp = R\x;
-%         sSpice = zeros(P,1);
-%         for m=1:P
-%             sSpice(m) = abs( p(m)*(A(:,m)'*tmp) );
-%         end
-%         tones(k,5) = min(sort(findpeaks(sSpice)));
-%         k = k+1;
-    end
-end
 
 %%
-figure
-semilogy(tones(:,1),tones(:,3),"x")
-hold on
-semilogy(tones(:,1),tones(:,4),"o")
-legend("Periodogram","Relax")
-scatter(tones(:,1),tones(:,2)./qtone,"r^")
-scatter(tones(:,1),tones(:,2).*qtone,"rV")
-
-%%
-voice = 1;
+voice = 5;
 track = naacor(:,voice);
 
 d    = 1; % peaks to look for
@@ -103,14 +50,13 @@ plotstop = 1500;
 for t=1:wnum-1
     %disp(t)
     x = track((t-1)*wlen+1:t*wlen);
-    %spect = abs( miaa(x,wlen,1:wlen,P) ).^2; % IAA, P must be >= 2* N
     spect = fftshift(abs(fft(x,P))/wlen).^2;   % periodogram
     spect = spect(P/2+1:end);
     if t == plotstop && ifplot
         plot(ff,spect)
         hold on
     end
-    if sum(spect) > 1e-4
+    if sum(spect) > 1e-3
         per = combFilter(spect,1,[],4, -0.01); % -0.01
         peaks(t,1) = ff(sort(findpeaks(per,1)));
     end
@@ -122,13 +68,9 @@ end
 
 figure
 plotparts(midinotes,voice)
-plot((wlen:wlen:N)./fs,peaks(:,1),".")
+plot((wlen:wlen:N)./fs,peaks,".")
+plot((wlen:wlen:N)./fs,movmedian(peaks,37),".")
 
 %%
-yyaxis left
-plot(real(txtcor(:,voice)))
-hold on
-yyaxis right
-for i=1:1
-    scatter((1:wnum)*length(txtcor)/length(peaks),movmedian(peaks(:,i),19))
-end
+
+
